dc4f952e5944ba0de171f2aa87eb5e8c74566ffc
Phillipe153 <rodrigues153@gmail.com>
2023-11-23 21:12:51 -0300

    feat: implementa sistema de combate cmompleto

--- components/ModalCombatResult.jsx
+++ components/ModalCombatResult.jsx
@@ -4,9 +4,13 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import HeroContext from "../context/HeroContext";
+import { green } from '@mui/material/colors';
+
+
 
 
 const style = {
+  color: '#fff',
   position: 'absolute',
   top: '50%',
   left: '50%',
@@ -17,20 +21,8 @@ const style = {
   boxShadow: 24,
   p: 12,
 };
-function Combat() {
-  const {firstHero, secondHero} = useContext(HeroContext)
-
-  const fisrtHeroScore = Object.values(firstHero.powerstats)
-  .reduce((accumulator, currentValue) => accumulator + currentValue);
-
-  const secondHeroScore = Object.values(secondHero.powerstats)
-  .reduce((accumulator, currentValue) => accumulator + currentValue);
-
-  const winner = fisrtHeroScore > secondHeroScore ? firstHero.name : secondHero.name;
-  
-  return winner;
-}
 export default function ModalCombatResult() {
+  const color = green[500];
 const {firstHero, secondHero,open, setOpen,setFirstHero,setSecondHero} = useContext(HeroContext)
   const handleClose = () => {
     setFirstHero(null);
@@ -51,32 +43,55 @@ const {firstHero, secondHero,open, setOpen,setFirstHero,setSecondHero} = useCont
 
   return (
     <div>
-      {/* <Button onClick={handleOpen}>Open modal</Button> */}
       <Modal
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
-        <h1>{winner}</h1>
+        <Box  sx={style}>
+          <h2><font color='green'> Winner </font> {winner}</h2>   
+        <div className='BoxStats'>
         <div  >
             <img  src={`${firstHero.images.sm}`} />             
-            <div>{secondHero.name}</div> 
+            <div>{firstHero.name}</div> 
         </div>
+        <div>
           {
             powerStats.map((stats) => (
-              <div key={stats}>
-                <div key={stats} color='#fffff'>{firstHero.powerstats[stats]}</div>
-                 <div key={stats} color='#fffff'>{stats}</div>
-                 <div key={stats} color='#fffff'>{secondHero.powerstats[stats]}</div>
+              <div className='stats' key={stats}>
+                <div className='stastValue'>
+                  {firstHero.powerstats[stats]}
+                  <div id='setaFirstHero'>
+                    {
+                      firstHero.powerstats[stats] > secondHero.powerstats[stats] 
+                      ? <img src='../seta_verde_para_esquerda.png'/> 
+                      : <img src='../seta_vermelha_para_direita.png'/>
+                    }
+                  </div>
+                </div>
+                 <div >{stats}</div>
+                 <div className='stastValue'>
+                  <div id='setaSecondHero'>
+                    {
+                      firstHero.powerstats[stats] > secondHero.powerstats[stats] 
+                      ? <img src='../seta_vermelha_para_esquerda.png'/> 
+                      : <img src='../seta_verde_para_direita.png'/>
+                    }
+                  </div>
+                    {secondHero.powerstats[stats]}
+                  </div>
               </div>
 
             ))
           }
+
+        </div>
         <div  >
             <img  src={`${secondHero.images.sm}`} />             
             <div color='#fff'>{secondHero.name}</div> 
+        </div>
+
         </div>
         </Box>
       </Modal>
